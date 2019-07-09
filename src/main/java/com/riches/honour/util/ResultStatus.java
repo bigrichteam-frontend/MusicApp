8997795e5f9758f9bf7c4c1938c2a0a7da002f00
want <2374909509@qq.com>
2019-07-09 11:36:00 +0800

    1： 实体bean的创建 2： 通用Mapper的继承 3： 测试通用mapper--》全部通过 王志坚 提交

---
+++ src/main/java/com/riches/honour/util/ResultStatus.java
@@ -0,0 +1,17 @@
+package com.riches.honour.util;
+
+/**
+ * @author 王志坚
+ * @createTime 2019.07.09.11:28
+ *
+ * 返回的
+ */
+public class ResultStatus {
+
+    public static Integer RESULT_PARAMS_ERROR = 205;      //参数错误
+    public static Integer RESULT_NAME_EXIST = 206;        //name 存在
+    public static Integer RESULT_OPERATE_ERROR = 255;     //操作失败
+    public static Integer RESULT_EXCEPTION_ERROR = 400;     //操作异常
+
+
+}
